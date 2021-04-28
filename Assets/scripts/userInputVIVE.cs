6af7a5debb653adf407a7767c5c198373296e805
Anna <anna.krucher@usys.ethz.ch>
2021-04-28 18:17:40 +0200

    trying out some things to access controller position

--- Assets/scripts/userInputVIVE.cs
+++ Assets/scripts/userInputVIVE.cs
@@ -10,8 +10,16 @@ public class userInputVIVE : MonoBehaviour
 
     public SteamVR_Action_Boolean Go_Cars = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("default", "CarsGO");
     public SteamVR_Action_Boolean Stop_Cars = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("default", "CarsSTOP");
+
+    [SerializeField]
+    GameObject contrleft;
+ 
     void Update()
+
     {
+        Vector3 posHMD = contrleft.transform.position;
+        Debug.Log(posHMD);
+
         if (Go_Cars.GetState(SteamVR_Input_Sources.Any))
         {
             _userAction.UpGo();
