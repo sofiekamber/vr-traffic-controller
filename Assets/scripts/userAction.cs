262009aefbba723670cdecfa87dcf83760b67a01
sofiekamber <sofie.kamber@gmail.com>
2021-05-26 19:45:38 +0200

    remove lane info logs

--- Assets/scripts/userAction.cs
+++ Assets/scripts/userAction.cs
@@ -11,49 +11,41 @@ public class userAction : MonoBehaviour
     public void UpGo()
     {
         lane_stop[0] = false;
-        Debug.Log("Lane 1 go");
     }
     public void UpStop()
     {
         lane_stop[0] = true;
-        Debug.Log("Lane 1 stop");
     }
 
     // Actions for LEFT lane
     public void LeftGo()
     {
         lane_stop[1] = false;
-        Debug.Log("Lane 2 go");
     }
     public void LeftStop()
     {
         lane_stop[1] = true;
-        Debug.Log("Lane 2 stop");
     }
 
     // Actions for DOWN lane
     public void DownGo()
     {
         lane_stop[2] = false;
-        Debug.Log("Lane 3 go");
     }
 
     public void DownStop()
     {
         lane_stop[2] = true;
-        Debug.Log("Lane 3 stop");
     }
 
      // Actions for RIGHT lane
     public void RightGo()
     {
         lane_stop[3] = false;
-        Debug.Log("Lane 4 go");
     }
     public void RightStop()
     {
         lane_stop[3] = true;
-        Debug.Log("Lane 4 stop");
     }
 
 }
